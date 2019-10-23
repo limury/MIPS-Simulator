b219d4469228a9eedcd50de38690e9d7767a35cb
Limury <zioberni222@gmail.com>
2019-10-23 16:35:22 +0100

    Typo

--- Memory.cpp
+++ Memory.cpp
@@ -1,6 +1,6 @@
 
 #include "Memory.h"
 
-Memory::Memory(const vector<const unsigned int>& input): executable(input), pc(0){}
+Memory::Memory(const vector<unsigned int>& input): executable(input), pc(0){}
 
 
