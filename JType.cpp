674440c6dc77d4b957d7239b95509abc9a039f76
Limury <zioberni222@gmail.com>
2019-11-02 17:11:32 +0000

    Slight adjustment to JTypes

--- JType.cpp
+++ JType.cpp
@@ -4,16 +4,17 @@ void Memory::J(int32_t x){
 
     int32_t target = x & 0x3FFFFFF;
 
+    int32_t tmp = pc & 0xF0000000;
     pc = npc;
-    npc = (target << 2) + 0x10000000;
+    npc = (target << 2) | tmp;
 
 }
 
 void Memory::JAL(int32_t x){
 
     int32_t target = x & 0x3FFFFFF;
-
+    int32_t tmp = pc & 0xF0000000;
     reg[31] = pc + 8;
     pc = npc;
-    npc = (target << 2) + 0x10000000;
+    npc = (target << 2) | tmp;
 }
